b582b9114857f776c230f9eef1b417908fcddaf0
kbehrens71 <46763582+kbehrens71@users.noreply.github.com>
2020-07-28 17:00:06 -0500

    added file save

--- AnimBatchSave.py
+++ AnimBatchSave.py
@@ -1,22 +1,7 @@
 # apply a given animation to a given character and save the file
-
 import maya.cmds
 import os
     
-# set file path
-# bring in anim  
-# attach
-# bake  
-
-anim_number = 0
-next_anim_path = ""
-start_time = 1
-end_time = 400
-
-def get_next_anim_path():
-    anim_number + 1
-    next_anim_path = "/Users/kaitlynbehrens/Documents/kaitlyn_maya_projects/maya_v2/scenes/sample_files/exercise/animations/maya/01_0{0}.ma".format(anim_number)
-
 def create_reference(file_path, ns):
     file_path_dir, file_path_fullname = os.path.split(file_path)
     file_path_name, file_path_ext = os.path.splitext(file_path_fullname)
@@ -27,14 +12,13 @@ def create_reference(file_path, ns):
 def get_joints_from_namespace(ns):
     return maya.cmds.ls("{0}:*".format(ns), type = "joint")
    
-
 anim_joints = get_joints_from_namespace("anim")
 char_joints = get_joints_from_namespace("character")
 
 def get_joint_name(ns_and_joint):
     ns, joint_name = ns_and_joint.split(":")
     return joint_name
-
+    
 def connect_joints():
     for c in char_joints:
         c_joint_name = get_joint_name(c)
@@ -42,41 +26,40 @@ def connect_joints():
             a_joint_name = get_joint_name(a)
             if c_joint_name == a_joint_name:
                 if "Hips" in c_joint_name:
-                    print "parent {0} and {1}".format("anim:" + a_joint_name, "character:" + c_joint_name)
+                    #print "parent {0} and {1}".format("anim:" + a_joint_name, "character:" + c_joint_name)
                     maya.cmds.parentConstraint("anim:" + a_joint_name, "character:" + c_joint_name)
                 elif ("Leg" in c_joint_name) or ("Foot" in c_joint_name):
                     maya.cmds.orientConstraint("anim:" + a_joint_name, "character:" + c_joint_name, mo = True)
                 else:
-                    print "orient {0} and {1}".format("anim:" + a_joint_name, "character:" + c_joint_name) 
+                    #print "orient {0} and {1}".format("anim:" + a_joint_name, "character:" + c_joint_name) 
                     maya.cmds.orientConstraint("anim:" + a_joint_name, "character:" + c_joint_name)
            
-
 # create new scene
 maya.cmds.file(new = True, force = True)
+maya.cmds.playbackOptions( minTime='0sec', maxTime='30sec')
+#maya.cmds.panZoom(z = 0.1)
+anim_number = 1
+start_time = 1
+end_time = 600
 
 # bring in character
 char_path = "/Users/kaitlynbehrens/Documents/kaitlyn_maya_projects/maya_v2/scenes/sample_files/exercise/character.mb"
 create_reference(char_path, "character")
 
 
-
 # bring in animation
-get_next_anim_path()
-if os.path.exists(next_anim_path):
-    anim_path = next_anim_path
+anim_path = "/Users/kaitlynbehrens/Documents/kaitlyn_maya_projects/maya_v2/scenes/sample_files/exercise/animations/maya/01_0{0}.ma".format(anim_number)
 create_reference(anim_path, "anim")
+
 
 # attach animation to character
 maya.cmds.parentConstraint("anim:Hips", "character:Reference")
 connect_joints()
 
 # bake animation onto joints
-#start_time = maya.cmds.playbackOptions(q = True, min = True)
-#end_time = maya.cmds.playbackOptions(q = True, max = True)
-print start_time
-print end_time
 maya.cmds.select(cl = True)
 maya.cmds.select(char_joints)
+maya.cmds.viewFit(f = .7)
 maya.cmds.bakeResults(
                         simulation = True,
                         time = (start_time, end_time),
@@ -91,15 +74,12 @@ maya.cmds.bakeResults(
                         controlPoints = False,
                         shape = True
                        )
-    
 
-                     
+# remove anim file
+maya.cmds.file( anim_path, rr = True )                       
+                       
 # save file
-
-
-
-for i in anim_joints:
-    print i
+renamed_file = "/Users/kaitlynbehrens/Documents/kaitlyn_maya_projects/maya_v2/scenes/sample_files/exercise/saved/SavedCharAnim_v{0}.mb".format(anim_number)
     
-for j in char_joints:
-    print j
+maya.cmds.file(rename = renamed_file)
+maya.cmds.file(save = True, f = True)

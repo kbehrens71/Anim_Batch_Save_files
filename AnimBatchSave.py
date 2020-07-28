fc5a04b88c645ef76837eea4b43103f4a96eee85
kbehrens71 <46763582+kbehrens71@users.noreply.github.com>
2020-07-28 18:34:59 -0500

    made to work with updated files

--- AnimBatchSave.py
+++ AnimBatchSave.py
@@ -2,6 +2,7 @@
 import maya.cmds
 import os
     
+       
 def create_reference(file_path, ns):
     file_path_dir, file_path_fullname = os.path.split(file_path)
     file_path_name, file_path_ext = os.path.splitext(file_path_fullname)
@@ -11,9 +12,18 @@ def create_reference(file_path, ns):
     
 def get_joints_from_namespace(ns):
     return maya.cmds.ls("{0}:*".format(ns), type = "joint")
+        
    
 anim_joints = get_joints_from_namespace("anim")
 char_joints = get_joints_from_namespace("character")
+
+rt_x_correction = -180
+rt_y_correction = 70
+rt_z_correction = 90
+
+lf_y_correction = -70
+lf_z_correction = -90
+
 
 def get_joint_name(ns_and_joint):
     ns, joint_name = ns_and_joint.split(":")
@@ -26,31 +36,40 @@ def connect_joints():
             a_joint_name = get_joint_name(a)
             if c_joint_name == a_joint_name:
                 if "Hips" in c_joint_name:
-                    #print "parent {0} and {1}".format("anim:" + a_joint_name, "character:" + c_joint_name)
                     maya.cmds.parentConstraint("anim:" + a_joint_name, "character:" + c_joint_name)
-                elif ("Leg" in c_joint_name) or ("Foot" in c_joint_name):
-                    maya.cmds.orientConstraint("anim:" + a_joint_name, "character:" + c_joint_name, mo = True)
-                else:
-                    #print "orient {0} and {1}".format("anim:" + a_joint_name, "character:" + c_joint_name) 
+                elif "Right" in c_joint_name:
+                    if "Leg" in c_joint_name:
+                        maya.cmds.orientConstraint("anim:" + a_joint_name, "character:" + c_joint_name, o = (rt_x_correction, 0, rt_z_correction))
+                    elif "Foot" in c_joint_name:
+                        maya.cmds.orientConstraint("anim:" + a_joint_name, "character:" + c_joint_name, o = (rt_x_correction, rt_y_correction, rt_z_correction))
+                    else:
+                        maya.cmds.orientConstraint("anim:" + a_joint_name, "character:" + c_joint_name, o = (rt_x_correction, 0, 0))
+                elif "Left" in c_joint_name:
+                    if "Leg" in c_joint_name:
+                        maya.cmds.orientConstraint("anim:" + a_joint_name, "character:" + c_joint_name, o = (0, 0, lf_z_correction))
+                    elif "Foot" in c_joint_name:
+                        maya.cmds.orientConstraint("anim:" + a_joint_name, "character:" + c_joint_name, o = (0, lf_y_correction, lf_z_correction))
+                    else:
+                        maya.cmds.orientConstraint("anim:" + a_joint_name, "character:" + c_joint_name)
+                else: 
                     maya.cmds.orientConstraint("anim:" + a_joint_name, "character:" + c_joint_name)
+ 
+ 
            
 # create new scene
 maya.cmds.file(new = True, force = True)
-maya.cmds.playbackOptions( minTime='0sec', maxTime='30sec')
-#maya.cmds.panZoom(z = 0.1)
-anim_number = 1
-start_time = 1
-end_time = 600
+maya.cmds.playbackOptions( minTime='0sec', maxTime='10sec')
+start_time = maya.cmds.playbackOptions(q = True, min = True)
+end_time = maya.cmds.playbackOptions(q = True, max = True)
+anim_number = 5
 
 # bring in character
-char_path = "/Users/kaitlynbehrens/Documents/kaitlyn_maya_projects/maya_v2/scenes/sample_files/exercise/character.mb"
+char_path = "/Users/kaitlynbehrens/Documents/kaitlyn_maya_projects/maya_v2/sample_files/exercise/character.mb"
 create_reference(char_path, "character")
 
-
 # bring in animation
-anim_path = "/Users/kaitlynbehrens/Documents/kaitlyn_maya_projects/maya_v2/scenes/sample_files/exercise/animations/maya/01_0{0}.ma".format(anim_number)
+anim_path = "/Users/kaitlynbehrens/Documents/kaitlyn_maya_projects/maya_v2/sample_files/exercise/animations/maya/01_0{0}.ma".format(anim_number)
 create_reference(anim_path, "anim")
-
 
 # attach animation to character
 maya.cmds.parentConstraint("anim:Hips", "character:Reference")
@@ -59,7 +78,7 @@ connect_joints()
 # bake animation onto joints
 maya.cmds.select(cl = True)
 maya.cmds.select(char_joints)
-maya.cmds.viewFit(f = .7)
+maya.cmds.viewFit(f = .8)
 maya.cmds.bakeResults(
                         simulation = True,
                         time = (start_time, end_time),
@@ -83,3 +102,4 @@ renamed_file = "/Users/kaitlynbehrens/Documents/kaitlyn_maya_projects/maya_v2/sc
     
 maya.cmds.file(rename = renamed_file)
 maya.cmds.file(save = True, f = True)
+
